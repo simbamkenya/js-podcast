9aa28bada3fee2c561783ff8c302d015bda90e43
simbankenya <pharesmuruthi@gmail.com>
2023-05-28 19:16:48 +0300

    added episoodes

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -1,43 +1,40 @@
 import React, { useEffect, useState } from 'react'
-import { collection, getDocs } from "firebase/firestore";
-import db from '../firebase';
-import Episode from './Episode';
-import Hosts from './Hosts';
-import Header from './Header';
+import { collection, getDocs } from 'firebase/firestore'
+import db from '../firebase'
+import Episode from './Episode'
+import Hosts from './Hosts'
+import Header from './Header'
 
 function Episodes() {
-    const [episodes, setEpisodes] = useState([]);
+  const [episodes, setEpisodes] = useState([])
 
-    const addEpisode = () => {
+  const fetchEpisodes = async () => {
+    await getDocs(collection(db, 'episodes'))
+      .then((episodeSnapshot) => {
+        const newEpisodes = episodeSnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
 
-    }
-
-    const fetchEpisodes = async () => {
-        await getDocs(collection(db, "episodes"))
-            .then((episodeSnapshot) => {
-                const newEpisodes = episodeSnapshot.docs
-                    .map((doc) => ({ ...doc.data(), id: doc.id }));
-
-                setEpisodes(newEpisodes);
-            }).catch()
-    }
-    useEffect(() => {
-      fetchEpisodes();
-    }, []);
-    console.log(episodes)
+        setEpisodes(newEpisodes)
+      })
+      .catch()
+  }
+  useEffect(() => {
+    fetchEpisodes()
+  }, [])
+  console.log(episodes)
 
   return (
     <>
-     <div className="bg-red-100 container mx-auto mt-2">
-      {/* <Hosts /> */}
-           <div className="space-y-4">
-                {episodes.map((episode, i) => (
-                    <Episode key={i} episode={episode} index={i} />                        
-                ))}
-                
-            </div>
-            
-    </div>
+      <div className="bg-red-100 container mx-auto mt-2">
+        {/* <Hosts /> */}
+        <div className="space-y-4">
+          {episodes.map((episode, i) => (
+            <Episode key={i} episode={episode} index={i} />
+          ))}
+        </div>
+      </div>
     </>
   )
 }
